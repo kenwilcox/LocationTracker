d419eca808a9747907dcaa0bbfd45018c530bd85
kenwilcox <wilcoxk@gmail.com>
2015-01-18 17:46:55 -0700

    Added Outlets

--- LocationTracker/ViewController.swift
+++ LocationTracker/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var theMap: MKMapView!
+  @IBOutlet weak var theLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
